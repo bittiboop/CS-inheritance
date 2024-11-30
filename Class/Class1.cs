9f3e84ae86f9c25a3ec5ac436132d19d33ddd328
bittiboop <sstrelkova15@gmail.com>
2024-11-30 13:36:21 +0200

    added ex-1

--- Class/Class1.cs
+++ Class/Class1.cs
@@ -1,8 +1,60 @@
 ﻿
 namespace CS_inheritance.Class
 {
-    internal class Class1
+    public class Human
     {
+        private string? _name;
+        private int? _age;
+        private string? _phonenum;
+        private string? _work;
 
+        public string Name
+        {
+            get => _name ?? "No Name";
+            set => _name = value;
+        }
+        public int Age
+        {
+            get => _age ?? 0;
+            set => _age = value;
+        }
+        public string Phonenum
+        {
+            get => _phonenum ?? "No Phone Number";
+            set => _phonenum = value;
+        }
+        public string Work
+        {
+            get => _work ?? "No Work";
+            set => _work = value;
+        }
+        public Human(string? name, int ?age, string? phonenum, string? work)
+        {
+            _name = name;
+            _age = age;
+            _phonenum = phonenum;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Phone Number: {Phonenum}";
+        }
+    }
+    public class Builder : Human
+    {
+        public Builder(string? name, int? age, string? phonenum) : base(name, age, phonenum, "Builder")
+        {
+        }
+    }
+    public class Sailor : Human
+    {
+        public Sailor(string? name, int? age, string? phonenum) : base(name, age, phonenum, "Sailor")
+        {
+        }
+    }
+    public class Pilot : Human
+    {
+        public Pilot(string? name, int? age, string? phonenum) : base(name, age, phonenum, "Pilot")
+        {
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace CS_inheritance
+﻿using CS_inheritance.Class;
+
+namespace CS_inheritance
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Our builders: ");
+            Builder builder = new Builder("John", 30, "1234567890");
+            Console.WriteLine(builder);
+            Console.WriteLine("Our sailors: ");
+            Sailor sailor = new Sailor("Jack", 40, "0987654321");
+            Console.WriteLine(sailor);
+            Console.WriteLine("Our pilots: ");
+            Pilot pilot = new Pilot("James", 50, "1357924680");
+            Console.WriteLine(pilot);
         }
     }
 }
